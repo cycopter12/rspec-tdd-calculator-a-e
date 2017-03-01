3561a9f412f08231b988cdb6d5a98f87ec5c63b9
elaine lim <lainex@gmail.com>
2017-03-01 15:12:39 +0800

    test1

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -3,8 +3,26 @@ require_relative '../lib/calculator'
 
 describe Calculator do
   before(:context) do
+    @my_cal = Calculator.new(1)
     # initialize calculator
+  end
+
+  describe "Initialization" do
+    it 'is an instance of Calculator' do
+      expect(@my_cal).to be_instance_of(Calculator)
+    end
   end
 
   # Tests go here
 end
+
+
+# describe Tree do
+#   before(:context) do
+#     @tree = Tree.new('oak', 65, 2)
+#   end
+#
+#   describe 'Initialization' do
+#     it 'is an instance of the Tree class' do
+#       expect(@tree).to be_instance_of(Tree)
+#     end
